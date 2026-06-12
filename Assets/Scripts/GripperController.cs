511e60fd24d86d9510d2be27f9812e704786f210
Ashley <ashleyriot000>
2026-06-12 17:19:22 +0900

    직교로봇 예제 추가

---
+++ Assets/Scripts/GripperController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GripperController : MonoBehaviour
+{
+    public List<Rigidbody> triggerList = new List<Rigidbody>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggerList.Contains(other.attachedRigidbody))
+            return;
+
+        triggerList.Add(other.attachedRigidbody);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!triggerList.Contains(other.attachedRigidbody))
+            return;
+
+        if (other.attachedRigidbody.isKinematic)
+            return;
+
+        triggerList.Remove(other.attachedRigidbody);
+    }
+
+    public void Grap(bool isGrap)
+    {
+        if(isGrap)
+        {
+            foreach(Rigidbody rb in triggerList)
+            {
+                rb.isKinematic = true;
+                rb.transform.SetParent(transform);
+            }
+        }
+        else
+        {
+            foreach (Rigidbody rb in triggerList)
+            {
+                rb.isKinematic = false;
+                rb.transform.SetParent(null);
+            }
+        }
+    }
+}
--- Assets/Scripts/MagneticSensor.cs
+++ Assets/Scripts/MagneticSensor.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 
 public class MagneticSensor : MonoBehaviour
 {
